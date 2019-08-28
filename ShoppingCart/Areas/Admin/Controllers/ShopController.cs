fe441afcad26ab71e475e933be9ce4708cab4384
Shehab Taher <Shehab Taher@work>
2019-08-28 16:13:14 +0200

    Product's List

--- ShoppingCart/Areas/Admin/Controllers/ShopController.cs
+++ ShoppingCart/Areas/Admin/Controllers/ShopController.cs
@@ -1,11 +1,15 @@
-﻿using ShoppingCart.Models.Data;
+﻿using PagedList;
+using ShoppingCart.Models.Data;
 using ShoppingCart.Models.ViewModels.Shop;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Web.WebPages;
 
 namespace ShoppingCart.Areas.Admin.Controllers
 {
@@ -23,12 +27,12 @@ namespace ShoppingCart.Areas.Admin.Controllers
                 categoryVMList = db.Categories
                                 .ToArray()
                                 .OrderBy(x => x.Sorting)
-                                .Select(x=> new CategoryVM(x))
+                                .Select(x => new CategoryVM(x))
                                 .ToList();
             }
 
-                //Return view with list
-                return View(categoryVMList);
+            //Return view with list
+            return View(categoryVMList);
         }
 
         // GET: Admin/Shop/AddNewCategory
@@ -61,5 +65,245 @@ namespace ShoppingCart.Areas.Admin.Controllers
             //Return id
             return id;
         }
+
+        // POST: Admin/Shop/RecordPages
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                //Set Initial count
+                int count = 1;
+
+                //Declare CategoryDTO
+                CategoryDTO dto;
+
+                //set sorting for each category
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+
+                    count++;
+                }
+
+            }
+        }
+
+        // GET: Admin/Shop/DeleteCategory/id
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Db db = new Db())
+            {
+                //Get the Category
+                CategoryDTO dto = db.Categories.Find(id);
+
+                //Remove the Category
+                db.Categories.Remove(dto);
+
+                //Save
+                db.SaveChanges();
+            }
+            //Redirect
+            return RedirectToAction("Categories");
+        }
+
+        // POST: Admin/Shop/RenameCategory
+        [HttpPost]
+        public string RenameCategory(string newCatName, int id)
+        {
+            using (Db db = new Db())
+            {
+                //check category name is unique
+                if (db.Categories.Any(x => x.Name == newCatName))
+                    return "titletaken";
+                //get DTO
+                CategoryDTO dto = db.Categories.Find(id);
+                //Edit DTO
+                dto.Name = newCatName;
+                dto.Slug = newCatName.Replace(" ", "-").ToLower();
+                //save
+                db.SaveChanges();
+            }
+            // return
+            return "ok";
+        }
+
+
+        // GET: Admin/Shop/AddProduct
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            //Init model
+            ProductVM model = new ProductVM();
+
+            //Add select list of categories to model
+            using(Db db = new Db())
+            {
+                model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+            }
+            //Return view with model
+            return View(model);
+        }
+
+        // POST: Admin/Shop/AddProduct
+        public ActionResult AddProduct(ProductVM model,HttpPostedFileBase file)
+        {
+            // Check model state
+            if (!ModelState.IsValid)
+            {
+                using (Db db = new Db())
+                {
+                    model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+                    return View(model);
+                }
+            }
+            // Make sure product name is unique
+            using (Db db = new Db())
+            {
+                if (db.Products.Any(x => x.Name == model.Name))
+                {
+                    model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+                    ModelState.AddModelError("", "The Product name is Taken!");
+                    return View(model);
+                }
+            }
+            // Declare product id
+            int id;
+
+            // Init and save productDTO
+            using (Db db = new Db())
+            {
+                ProductDTO product = new ProductDTO();
+
+                product.Name = model.Name;
+                product.Slug = model.Name.Replace(" ","-").ToLower();
+                product.Description = model.Description;
+                product.Price = model.Price;
+                product.CategoryId = model.CategoryId;
+
+                CategoryDTO catDTO = db.Categories.FirstOrDefault(x => x.Id == model.CategoryId);
+                model.CategoryName = catDTO.Name;
+
+                db.Products.Add(product);
+                db.SaveChanges();
+
+                // Get the id
+                id = product.Id;
+            }
+
+            // Set TempDat message
+            TempData["SM"] = "You Have added a product!";
+
+            #region Upload Image
+            // Create neccessary directories
+            var originalDirectory = new DirectoryInfo(string.Format("{0}Images\\Upload",Server.MapPath(@"\")));
+
+            var pathString1 = Path.Combine(originalDirectory.ToString(), "Products");
+            var pathString2 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString());
+            var pathString3 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString()+"\\Thumbs");
+            var pathString4 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery");
+            var pathString5 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery\\Thumbs");
+
+            if (!Directory.Exists(pathString1))
+                Directory.CreateDirectory(pathString1);
+
+
+            if (!Directory.Exists(pathString2))
+                Directory.CreateDirectory(pathString2);
+
+
+            if (!Directory.Exists(pathString3))
+                Directory.CreateDirectory(pathString3);
+
+            if (!Directory.Exists(pathString4))
+                Directory.CreateDirectory(pathString4);
+
+            if (!Directory.Exists(pathString5))
+                Directory.CreateDirectory(pathString5);
+
+            // Check if a file was uploaded
+            if(file != null && file.ContentLength > 0)
+            {
+
+                // Get file extension
+                string ext = file.ContentType.ToLower();
+
+                // Verify extension
+                if (ext != "image/jpg" &&
+                    ext != "image/jpeg" &&
+                    ext != "image/pjpeg" &&
+                    ext != "image/gif" &&
+                    ext != "image/x-png" &&
+                    ext != "image/png")
+                {
+                    using (Db db = new Db())
+                    {
+                       
+                            model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+                            ModelState.AddModelError("", "The Image was not uploaded - wrong image extension");
+                            return View(model);
+                        
+                    }
+                }
+                // Init image name
+                string imageName = file.FileName;
+                // Save image name to DTO
+                using(Db db = new Db())
+                {
+                    ProductDTO dto = db.Products.Find(id);
+                    dto.ImageName = imageName;
+
+                    db.SaveChanges();
+                }
+                // Set original and thumb image paths
+                var path = string.Format("{0}\\{1}", pathString2, imageName);
+                var path2 = string.Format("{0}\\{1}", pathString3, imageName);
+
+                // Save original
+                file.SaveAs(path);
+
+                // Create and save thumb
+                WebImage img = new WebImage(file.InputStream);
+                img.Resize(200, 200);
+                img.Save(path2);
+
+            }
+            #endregion
+            return RedirectToAction("AddProduct");
+        }
+
+        // GET: Admin/Shop/Products
+        public ActionResult Products(int? page , int? catId)
+        {
+            // Declare a list if ProductVM
+            List<ProductVM> listOfProductVM;
+
+            // Set page Number
+            var pageNumber = page ?? 1;
+
+            using(Db db = new Db()) {
+                // Init the list
+                listOfProductVM = db.Products.ToArray()
+                                    .Where(x => catId == null || catId == 0 || x.CategoryId == catId)
+                                    .Select(x => new ProductVM(x))
+                                    .ToList();
+
+                // Populate categories select list
+                ViewBag.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+
+                // Set selected category
+                ViewBag.SelectCat = catId.ToString();
+            }
+
+            // Set Pagination
+            var onePageOfProducts = listOfProductVM.ToPagedList(pageNumber,3);
+            ViewBag.OnePageOfProducts = onePageOfProducts;
+            // Return view with list
+            return View(listOfProductVM);
+        }
+        
     }
 }
